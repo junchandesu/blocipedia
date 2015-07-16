43300baec23e65d380be5c2ff7fabcf1d15c891c
Junko Werner <aliciajace@gmail.com>
2015-07-15 20:05:47 -0700

    Completed Stripe with block suggestions

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,13 +1,12 @@
 class ChargesController < ApplicationController 
 
    def new
-     @stripe_btn_data = {
-       key: "#{ Rails.configuration.stripe[:publishable_key] }",
-       description: "BigMoney Membership - #{current_user.name}",
-       amount:  @default_amount 
-     }
-     @default_amount = 150_00
-   end
+   @stripe_btn_data = {
+     key: "#{ Rails.configuration.stripe[:publishable_key] }",
+     description: "BigMoney Membership - #{current_user.name}",
+     amount: Amount.default
+   }
+ end
 
 
    def downgrade
@@ -20,36 +19,32 @@ class ChargesController < ApplicationController
       
     end
 
-  def create
-    @default_amount = 150_00
+ def create
+ 
    # Creates a Stripe Customer object, for associating
    # with the charge
-     customer = Stripe::Customer.create(
-       email: current_user.email,
-       card: params[:stripeToken]
-      )
+   customer = Stripe::Customer.create(
+     email: current_user.email,
+     card: params[:stripeToken]
+   )
  
    # Where the real magic happens
-    charge = Stripe::Charge.create(
-       customer: customer.id, # Note -- this is NOT the user_id in your app
-       amount:  @default_amount, 
-       description: "BigMoney Membership - #{current_user.email}",
-       currency: 'usd'
-     )
-    current_user.update_attributes(role: "premium")
-    flash[:success] = "Thanks for all the money, #{current_user.email}! Feel free to pay me again."
-    redirect_to user_path(current_user) # or wherever
-
-
-   
-
+   charge = Stripe::Charge.create(
+     customer: customer.id, # Note -- this is NOT the user_id in your app
+     amount: Amount.default,
+     description: "BigMoney Membership - #{current_user.email}",
+     currency: 'usd'
+   )
+ 
+   flash[:success] = "Thanks for all the money, #{current_user.email}! Feel free to pay me again."
+   redirect_to user_path(current_user) # or wherever
  
  # Stripe will send back CardErrors, with friendly messages
  # when something goes wrong.
  # This `rescue block` catches and displays those errors.
-   rescue Stripe::CardError => e
-    flash[:error] = e.message
-    redirect_to new_charge_path
-   end
+ rescue Stripe::CardError => e
+   flash[:error] = e.message
+   redirect_to new_charge_path
+ end
 
 end
--- app/models/amount.rb
+++ app/models/amount.rb
@@ -1,5 +1,5 @@
-class Amount
+class Amount < ActiveRecord::Base
 	def default
-	 return 10_00
+	 10_00
 	end
 end
